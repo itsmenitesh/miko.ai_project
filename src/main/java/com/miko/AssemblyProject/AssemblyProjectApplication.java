e4d44ad2a1511b8d28ae2631a734e0f5f9a2d08a
nitesh choudhary <niteshchoudhary1217@gmail.com>
2024-02-06 00:55:58 +0530

    initial commit

---
+++ src/main/java/com/miko/AssemblyProject/AssemblyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.miko.AssemblyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyProjectApplication.class, args);
+	}
+
+}
